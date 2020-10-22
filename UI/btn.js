3b16f05ff3acfa539d2705d7d8be5f02f4c7c329
Mukesh Jaiswal <56516302+MukeshJaiswal01@users.noreply.github.com>
2020-10-22 23:15:30 +0530

    Add files via upload

---
+++ UI/btn.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "180px";
+    document.getElementById("main").style.display = "none";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+
+    document.getElementById("main").style.display = "block";
+  }
